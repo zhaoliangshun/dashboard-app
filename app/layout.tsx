94f4aa9d986116ddef23e5cb3726bee03d14d7ab
Your Name <you@example.com>
2025-08-20 06:36:05 +0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        
+        赵良顺，你好啊，真好，可以好好开始学习了，哈哈哈哈哈哈，你好我好，真好
+        {children}
+
+
+
+      </body>
     </html>
   );
 }
